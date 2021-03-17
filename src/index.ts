ca07bedd6b5494885c740e2c3f46a2f2bf225f5a
Sebastian Hölscher <sebastian@hoelshare.de>
2021-03-17 19:36:06 +0100

    Add deployment route

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ declare interface iAppTemplateOptions {
     appUpdatedRoute?: string,
     appActivatedRoute?: string,
     appDeactivatedRoute?: string,
+    appDeploymentRoute: string,
     appSecret: string,
     appName: string
 }
@@ -123,7 +124,7 @@ class AppTemplate extends EventEmitter {
             name,
             shopSecret,
             appSecret: this.options.appSecret,
-            confirmationUrl: `http://localhost:8000${this.options.confirmRoute}`
+            confirmationUrl: `${this.options.appDeploymentRoute}${this.options.confirmRoute}`
         });
 
         response.json(proof);
